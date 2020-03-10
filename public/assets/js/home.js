1743ba32e41a7f40cb31ec5dd879148a59fdad20
kellarelliott <kellarelliott@gmail.com>
2020-03-10 03:22:10 +0000

    function setAuth

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+const $setLogin = $('#login');
+const $setSignUp = $('#signup');
+
+let authSetting = 'login';
+
+function setAuth(setting) {
+  authSetting = setting;
+
+  if (authSetting === 'login') {
+    $setLogin.addClass('active');
+    $setSignUp.removeClass('active');
+  } else {
+    $setSignUp.addClass('active');
+    $setLogin.removeClass('active');
+  }
+}
+
+$setLogin.on('click', setAuth.bind(null, 'login'));
+$setSignUp.on('click', setAuth.bind(null, 'signup'));
