4ce7d395ab0fb2cdfa0b5f54bb2a7af26e3ee55b
kellarelliott <kellarelliott@gmail.com>
2020-03-11 15:44:33 +0000

    board.js MicroModal

--- public/assets/js/board.js
+++ public/assets/js/board.js
@@ -1,5 +1,8 @@
 const $logoutButton = $('#logout');
 const $boardContainer = $('.container');
+const $boardName = $('header > h1');
+const $createListInput = $('#create-list input');
+const $saveListButton = $('#create-list .save');
 
 let board;
 
@@ -47,14 +50,52 @@ function createLists(lists) {
     return $listContainer;
   });
 
+  let $addListContainer = $('<div class="list add">');
+  let $addListButton = $('<button>')
+    .text('+ Add another list')
+    .on('click', openListCreateModal);
+
+  $addListContainer.append($addListButton);
+  $listContainers.push($addListContainer);
   return $listContainers;
 }
 
 function renderBoard() {
   let $lists = createLists(board.lists);
 
+  $boardName.text(board.name);
+
   $boardContainer.empty();
   $boardContainer.append($lists);
 }
 
+function openListCreateModal() {
+  $createListInput.val('');
+  MicroModal.show(`create-list`);
+}
+
+function handleListCreate(event) {
+  event.preventDefault();
+
+  let listTitle = $createListInput.val().trim();
+
+  if (!listTitle) {
+    MicroModal.close('create-list');
+    return;
+  }
+
+  $.ajax({
+    url: '/api/lists',
+    method: 'POST',
+    data: {
+      board_id: board.id,
+      title: listTitle
+    }
+  }).then(function () {
+    init();
+    MicroModal.close('create-list');
+  });
+}
+
+$saveListButton.on('click', handleListCreate);
 $logoutButton.on('click', handleLogout);
